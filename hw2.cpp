51e4fce14b5ec805381d8c9737900c13d0b56ec8
richarkc <richarkc@ceclnx01.cec.miamioh.edu>
2016-02-16 09:18:46 -0500

    Fixed stylistic errors and added comments

--- hw2.cpp
+++ hw2.cpp
@@ -1,41 +1,43 @@
-// Copyright (C) 2016 ?????
+// Copyright (C) 2016 Kyle Richardson
 
 #include <iostream>
 #include <algorithm>
 #include <string>
 #include <cctype>
 
-
-void putChar(std::string& s, const int targetPos, const int oldPos, const char c, const int num){
+void putChar(std::string& s, const int targetPos,
+  const int oldPos, const char c, const int num) {
+    // two seperate cases for before and after gather pos
     if (oldPos < targetPos) {
-        for (int i = oldPos; i < targetPos - 1; i++){
-				s[i] = s[i + 1];
-			            
+        for (int i = oldPos; i < targetPos - 1; i++) {
+          s[i] = s[i + 1];
         }
-		s[targetPos - 1] = s[targetPos + num];
-        s[targetPos + num] = c;
-    }
-    else if (targetPos < oldPos) {
-		int i = oldPos;
-        for (int i = oldPos; i > (targetPos + num); i--){
-				s[i] = s[i - 1];
+    // move the next character after the gather before it to maintain weight
+    s[targetPos - 1] = s[targetPos + num];
+    s[targetPos + num] = c;
+    } else if (targetPos < oldPos) {
+        for (int i = oldPos; i > (targetPos + num); i--) {
+          s[i] = s[i - 1];
         }
         s[targetPos + num] = c;
     }
 }
 
 void gatherDigits(std::string& s, const int pos) {
-    int num = 0;
-	int i = 0;
-    for (i; i < s.length(); i++){
-		if (i >= pos && i < (pos + num)) {
-			continue;
-		}
-        if (std::isdigit(s[i])){
-            putChar(s,pos,i,s[i], num);
+  int num = 0;
+  // i cannot be negative, so unsigned cast for comparison
+  for (int i = 0; (unsigned)i < s.length(); i++) {
+    // ignore all ints already "gathered"
+    if (i >= pos && i < (pos + num)) {
+      continue;
+    }
+        if (std::isdigit(s[i])) {
+            putChar(s, pos, i, s[i], num);
             num++;
-			if (i < pos)
-				i--;
+      // ensure case where 2 ints next to each other will still
+      // be checked when swapped
+      if (i < pos)
+        i--;
         }
     }
 }
